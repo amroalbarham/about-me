9bc47eeb67c177cc54640f397a01bd206d9920a0
amroalbarham <amroalbarham@gmail.com>
2021-03-30 16:35:57 +0300

    finish lab

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LAB 02 
+## LAB 02 
 
 
 ## project name : about-me
@@ -18,3 +18,18 @@
 *i learn a new something how to Trace the lines to discover the problem.*
 + **How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?**
 *it takes about three hours and expected that*
+
+## LAB 03 
+
+
+**today i write a nice code that I used for the loop to give user four attempts to guess the number and I use an array to another game and use a nested loop for this array one of them to count the attempts and another to access to array and search inside it, and I used the variable to count the number of correct answers.**
+
+
+### Reflections and Comments :
++ **How did this go, overall?** 
+*it's good but I need to focus more than next time*
+
++ **What observations or questions do you have about what you’ve learned so far?** 
+*i learned from write code we need to be attention for everything in the code that's about declare a variables and semicolon and parentheses*
++ **How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?**
+*it takes about two and half hours hours and expected that*
--- js/app.js
+++ js/app.js
@@ -1,87 +1,157 @@
-'use strict'
+'use strict';
 
 
-let userName=prompt('please write the name  ');
+let score = 0;
+let userName = prompt('please write the name  ');
 // console.log(userName);
-alert('you are welcome in my website Mr '+userName);
+alert('you are welcome in my website Mr ' + userName);
 
-let farmer=prompt('are you a farmer?');
-farmer=farmer.toLocaleLowerCase();
+let farmer = prompt('Am I a farmer?');
+farmer = farmer.toLocaleLowerCase();
 
-if (farmer=='y' || farmer=='yes'){
+if (farmer == 'y' || farmer == 'yes') {
         // console.log(farmer);
-        alert('you are a farmer');
+        alert('yes great i\'m');
+        score += 1;
 
-}else if(farmer=='n'|| farmer=='no'){
+} else if (farmer == 'n' || farmer == 'no') {
         // console.log(farmer);
-        alert('you are not a farmer !');
-}else{
+        alert('no i\'m a farmer !');
+} else {
         // console.log(farmer);
         alert('please enter yes or no or y or n');
 }
 
 
 
-let player=prompt('are you love Nadal ?');
-player=player.toLocaleLowerCase();
+let player = prompt('Am I love Nadal ?');
+player = player.toLocaleLowerCase();
 
-if (player=='y' || player=='yes'){
+if (player == 'y' || player == 'yes') {
         // console.log(player);
-        alert('your favourite plater is nadal ');
+        alert('correct,i love nadal ');
+        score += 1;
 
-}else if(player=='n'|| player=='no'){
+} else if (player == 'n' || player == 'no') {
         // console.log(player);
-        alert('your favourite plater isn\'t nadal!');
-}else{
+        alert('wrong answer ');
+} else {
         // console.log(player);
         alert('please enter yes or no or y or n');
 }
 
 
-let weather=prompt('are you love the hot weather?');
-weather=weather.toLocaleLowerCase();
+let weather = prompt('Do I like the weather??');
+weather = weather.toLocaleLowerCase();
 
-if (weather=='y' || weather=='yes'){
+if (weather == 'y' || weather == 'yes') {
         // console.log(weather);
-        alert('you are love hot weather');
+        alert('yes i love hot weather');
+        score += 1;
 
-}else if(weather=='n'|| weather=='no'){
+} else if (weather == 'n' || weather == 'no') {
         // console.log(weather);
-        alert('you are don\'t love hot weather');
-}else{
+        alert('no i don\'t love hot weather');
+} else {
         // console.log(weather);
         alert('please enter yes or no or y or n');
 }
 
 
-let brothers=prompt('have you brothers?');
-brothers=brothers.toLocaleLowerCase();
+let brothers = prompt('Do I have brothers?');
+brothers = brothers.toLocaleLowerCase();
 
-if (brothers=='y' || brothers=='yes'){
+if (brothers == 'y' || brothers == 'yes') {
         // console.log(brothers);
-        alert('you are have brothers');
+        alert('yes i  have one brother');
+        score += 1;
 
-}else if(brothers=='n'|| brothers=='no'){
+} else if (brothers == 'n' || brothers == 'no') {
         // console.log(brothers);
-        alert('you are don\'t have brothers');
-}else{
+        alert('no i don\'t have brothers');
+} else {
         // console.log(brothers);
         alert('please enter yes or no or y or n');
 }
-let stars=prompt('are you love stars?');
-stars=stars.toLocaleLowerCase();
+let stars = prompt('Do I love the stars?');
+stars = stars.toLocaleLowerCase();
 
-if (stars=='y' || stars=='yes'){
+if (stars == 'y' || stars == 'yes') {
         // console.log(stars);
-        alert('you are love stars');
-
-}else if(stars=='n'|| stars=='no'){
+        alert('yes i love stars');
+        score += 1;
+} else if (stars == 'n' || stars == 'no') {
         // console.log(stars);
-        alert('you are don\'t love stars');
-}else{
+        alert('no i don\'t  love stars');
+} else {
         // console.log(stars);
         alert('please enter yes or no or y or n');
 }
 
-alert('hope you are enjoyed Mr '+userName+' in my website,have a nice day ');
+
+
+let number = 5;
+
+for (let i = 0; i <= 3; i++) {
+
+        let guessingNumber = prompt('please enter the number ?');
+        guessingNumber = parseInt(guessingNumber);
+
+        if (guessingNumber === number) {
+                score++;
+                alert('great you answer correct');
+                break;
+        }
+        else if (guessingNumber > number) {
+
+                alert('the number is too high');
+
+        }
+
+        else if (guessingNumber < number) {
+                alert('the nunber is too low');
+        }
+        if (i === 3) {
+                alert(' hard luck ,the correct answer is 5');
+        }
+
+}
+
+
+let playerList = ['NOVAK', 'DANIIL', 'NADAL', 'DOMINIC', 'DOMINIC', 'FEDERER', 'ZVEREV'];
+for (let i = 0; i < 6; i++) {
+
+        
+        let userEnter = prompt('try to guess the player');
+        userEnter = userEnter.toUpperCase();
+
+        for (let j = 0; j < playerList.length; j++) {
+
+                console.log(j);
+                if (playerList[j] === userEnter) {
+                        alert('great man correct answer');
+                        score++;
+                        
+                        i =7;
+                        break;
+                        // console.log();
+                }
+
+
+        }
+       
+
+}
+
+alert("the answer sheet\n NOVAK or DANIIL or NADAL or DOMINIC or  DOMINIC or FEDERER or ZVEREV ");
+
+
+
+
+
+
+
+
+
+alert(`hope you are enjoyed Mr ${userName} in my website,have a nice day  and your score is ${score} out of 7`);
 
